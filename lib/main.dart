f099521f945a7fe9cdac8c39c450058e020870f4
Amine BENBAKHTA <gm_benbakhta@esi.dz>
2020-04-18 21:01:02 +0100

    Completing the challenge

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,37 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: SafeArea(),
+        body: SafeArea(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                width: 100.0,
+                color: Colors.red,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    height: 100.0,
+                    width: 100.0,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    height: 100.0,
+                    width: 100.0,
+                    color: Colors.green[300],
+                  )
+                ],
+              ),
+              Container(
+                width: 100.0,
+                color: Colors.blue,
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
