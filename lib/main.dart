53f02e0e9773303fd4271062d0a69e1a669a5009
Amine BENBAKHTA <gm_benbakhta@esi.dz>
2020-04-18 20:51:09 +0100

    Using Columns and Rows

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,77 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            //margin: EdgeInsets.all(20.0),
-            //margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
-            margin: EdgeInsets.only(left: 30.0),
-            padding: EdgeInsets.all(20.0),
-            color: Colors.white,
-            child: Text('Hello'),
+//          child: Column(
+//            //mainAxisSize: MainAxisSize.min,
+//            //verticalDirection: VerticalDirection.up,
+//            //mainAxisAlignment: MainAxisAlignment.center,
+//            //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//            //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//            //crossAxisAlignment: CrossAxisAlignment.end,
+//            crossAxisAlignment: CrossAxisAlignment.stretch,
+//            children: <Widget>[
+//              Container(
+//                height: 100.0,
+//                width: 100.0,
+//                //margin: EdgeInsets.all(20.0),
+//                //margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
+//                //margin: EdgeInsets.only(left: 30.0),
+//                //padding: EdgeInsets.all(20.0),
+//                color: Colors.green,
+//                child: Text('Container 1'),
+//              ),
+//              SizedBox(
+//                height: 20.0,
+//              ),
+//              Container(
+//                height: 100.0,
+//                width: 300.0,
+//                color: Colors.red,
+//                child: Text('Container 2'),
+//              ),
+//              Container(
+//                height: 100.0,
+//                width: 100.0,
+//                color: Colors.white,
+//                child: Text('Container 3'),
+//              ),
+////              //Container(
+////                width: double.infinity,
+////                height: 10.0,
+////              )
+//            ],
+//          ),
+          child: Row(
+            //mainAxisSize: MainAxisSize.min,
+            //verticalDirection: VerticalDirection.up,
+            //mainAxisAlignment: MainAxisAlignment.center,
+            //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //crossAxisAlignment: CrossAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                //margin: EdgeInsets.all(20.0),
+                //margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
+                //margin: EdgeInsets.only(left: 30.0),
+                //padding: EdgeInsets.all(20.0),
+                width: 30.0,
+                color: Colors.green,
+                child: Text('Container 1'),
+              ),
+              SizedBox(
+                width: 30.0,
+              ),
+              Container(
+                color: Colors.red,
+                child: Text('Container 2'),
+              ),
+              Container(
+                width: 100.0,
+                color: Colors.white,
+                child: Text('Container 3'),
+              ),
+            ],
           ),
         ),
       ),
