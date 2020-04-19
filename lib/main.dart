cb53f9a8316dc7510f8bd5807019ec53e074e081
Amine BENBAKHTA <gm_benbakhta@esi.dz>
2020-04-19 19:45:03 +0100

    Manipulating the icon widget

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,55 @@ class MyApp extends StatelessWidget {
                   fontFamily: 'Source Sans Pro',
                   letterSpacing: 2.0,
                 ),
-              )
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+2137 77 10 59 66',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.mail,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'gm_benbakhta@esi.dz',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ],
           ),
         ),
