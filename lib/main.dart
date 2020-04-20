0d35228632d2ce87830f0dc61170411783c196de
Amine BENBAKHTA <gm_benbakhta@esi.dz>
2020-04-20 17:08:01 +0100

    Adding FlatButtons

--- lib/main.dart
+++ lib/main.dart
@@ -23,16 +23,20 @@ class DicePage extends StatelessWidget {
         children: <Widget>[
           Expanded(
             //flex: 2,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
               child: Image.asset('images/dice1.png'),
+              onPressed: () {
+                print('Left button pressed.');
+              },
             ),
           ),
           Expanded(
             //flex: 1,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
               child: Image.asset('images/dice2.png'),
+              onPressed: () {
+                print('Right button pressed.');
+              },
             ),
           ),
         ],
