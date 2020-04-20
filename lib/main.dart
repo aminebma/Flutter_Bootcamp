4754e41fbac43e2089fc179f3951dfb27b8d8c49
Amine BENBAKHTA <gm_benbakhta@esi.dz>
2020-04-21 00:33:28 +0100

    Downloading a flutter package and testing it

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -82,78 +83,79 @@ class MyApp extends StatelessWidget {
 //              ),
 //            ],
 //          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundColor: Colors.red,
-                backgroundImage: AssetImage('images/amine.png'),
-              ),
-              Text(
-                'Amine BENBAKHTA',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Pacifico',
-                    letterSpacing: 2.5),
-              ),
-              Text(
-                'PROJECT MANAGER',
-                style: TextStyle(
-                  color: Colors.teal[100],
-                  fontSize: 25.0,
-                  fontWeight: FontWeight.w400,
-                  fontFamily: 'Source Sans Pro',
-                  letterSpacing: 2.0,
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-                width: 150.0,
-                child: Divider(
-                  color: Colors.teal[100],
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
-                    color: Colors.teal,
-                  ),
-                  title: Text(
-                    '+2137 77 10 59 66',
-                    style: TextStyle(
-                      color: Colors.teal.shade900,
-                      fontFamily: 'Source Sans Pro',
-                      fontSize: 20.0,
-                    ),
-                  ),
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.mail,
-                    color: Colors.teal,
-                  ),
-                  title: Text(
-                    'gm_benbakhta@esi.dz',
-                    style: TextStyle(
-                      color: Colors.teal.shade900,
-                      fontFamily: 'Source Sans Pro',
-                      fontSize: 20.0,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
+//          child: Column(
+//            mainAxisAlignment: MainAxisAlignment.center,
+//            children: <Widget>[
+//              CircleAvatar(
+//                radius: 50.0,
+//                backgroundColor: Colors.red,
+//                backgroundImage: AssetImage('images/amine.png'),
+//              ),
+//              Text(
+//                'Amine BENBAKHTA',
+//                style: TextStyle(
+//                    color: Colors.white,
+//                    fontSize: 30.0,
+//                    fontWeight: FontWeight.bold,
+//                    fontFamily: 'Pacifico',
+//                    letterSpacing: 2.5),
+//              ),
+//              Text(
+//                'PROJECT MANAGER',
+//                style: TextStyle(
+//                  color: Colors.teal[100],
+//                  fontSize: 25.0,
+//                  fontWeight: FontWeight.w400,
+//                  fontFamily: 'Source Sans Pro',
+//                  letterSpacing: 2.0,
+//                ),
+//              ),
+//              SizedBox(
+//                height: 20.0,
+//                width: 150.0,
+//                child: Divider(
+//                  color: Colors.teal[100],
+//                ),
+//              ),
+//              Card(
+//                color: Colors.white,
+//                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+//                child: ListTile(
+//                  leading: Icon(
+//                    Icons.phone,
+//                    color: Colors.teal,
+//                  ),
+//                  title: Text(
+//                    '+2137 77 10 59 66',
+//                    style: TextStyle(
+//                      color: Colors.teal.shade900,
+//                      fontFamily: 'Source Sans Pro',
+//                      fontSize: 20.0,
+//                    ),
+//                  ),
+//                ),
+//              ),
+//              Card(
+//                color: Colors.white,
+//                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+//                child: ListTile(
+//                  leading: Icon(
+//                    Icons.mail,
+//                    color: Colors.teal,
+//                  ),
+//                  title: Text(
+//                    'gm_benbakhta@esi.dz',
+//                    style: TextStyle(
+//                      color: Colors.teal.shade900,
+//                      fontFamily: 'Source Sans Pro',
+//                      fontSize: 20.0,
+//                    ),
+//                  ),
+//                ),
+//              ),
+//            ],
+//          ),
+          child: Center(child: Text(nouns.first)),
         ),
       ),
     );
